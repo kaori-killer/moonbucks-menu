4b4cf5b3e87b0e8453689cc1ac7ce3f8f0cd529e
kaori-killer <sksbthwjd@daum.net>
2023-02-20 21:00:18 +0900

    feat: 추가되는 메뉴의 아래 마크업은 ul태그 안에 삽입

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@
 
 // TODO 메뉴 추가
 // - [x] 메뉴의 이름을 입력 받고 엔터키 입력으로 추가한다.
-// - 추가되는 메뉴의 아래 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
+// - [x] 추가되는 메뉴의 아래 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
 // - [ ] 총 메뉴 갯수를 count하여 상단에 보여준다.
 //   - [ ] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
 //   - [ ] 사용자 입력값이 빈 값이라면 추가되지 않는다.
@@ -15,9 +15,26 @@ function App() {
             e.preventDefault();
     });
 
+    // 메뉴의 이름을 입력받는건
     $("#espresso-menu-name").addEventListener("keypress", (e)=>{
         if(e.key === "Enter") {
-            console.log(document.querySelector("#espresso-menu-name").value);
+            const espressMenuName = $("#espresso-menu-name").value;
+            const menuItemTemplate = (espressMenuName) => { return `<li class="menu-list-item d-flex items-center py-2">
+                <span class="w-100 pl-2 menu-name">${espressMenuName}</span>
+                <button
+                type="button"
+                class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+                >
+                수정
+                </button>
+                <button
+                type="button"
+                class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+                >
+                삭제
+                </button>
+            </li>`};
+            $("#espresso-menu-list").innerHTML += menuItemTemplate(espressMenuName);
         }
     });
 }
