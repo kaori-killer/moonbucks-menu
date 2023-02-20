7fbf67b29d98657bc27953a6eee41f13271cbfeb
kaori-killer <sksbthwjd@daum.net>
2023-02-20 22:34:25 +0900

    feat: 총 메뉴 갯수를 count하여 상단에 보여준다

--- src/js/index.js
+++ src/js/index.js
@@ -12,14 +12,18 @@
 // - [x] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하는 모달창(prompt)이 뜬다.
 // - [x] 모달창에서 신규메뉴명을 입력 받고, 확인 버튼을 누르면 메뉴가 수정된다.    
     
+// TODO 메뉴 삭제
+// - [x] 메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌 모달창이 뜬다.
+// - [x] 확인 버튼을 클릭하면 메뉴가 삭제된다.
+// - [x] 총 메뉴 갯수를 count하여 상단에 보여준다.
 
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
-    // TODO 메뉴 삭제
-    // - [x] 메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌 모달창이 뜬다.
-    // - [x] 확인 버튼을 클릭하면 메뉴가 삭제한다.
-    // - [ ] 총 메뉴 갯수를 count하여 상단에 보여준다.
+    const countMenuAmount= () => {
+        const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+        $(".menu-count").innerText = `총 ${menuCount}개`
+    }
 
     $("#espresso-menu-list").addEventListener("click", (e)=>{
         console.log(e.target.classList);
@@ -31,6 +35,7 @@ function App() {
         if(e.target.classList.contains("menu-remove-button")){
             if(confirm("정말 삭제하시겠습니까?")){
                 e.target.closest("li").remove();
+                countMenu();
             }
         }
     })
@@ -64,8 +69,7 @@ function App() {
             "beforeend",
             menuItemTemplate(espressMenuName)
         );
-        const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
-        $(".menu-count").innerText = `총 ${menuCount}개`
+        countMenuAmount();
         $("#espresso-menu-name").value = "";
     }
 
