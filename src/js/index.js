682bf620804040da6c961f25b7a99290249a9402
kaori-killer <sksbthwjd@daum.net>
2023-02-20 21:28:53 +0900

    refactor: 메뉴의 이름을 입력 받을 때, 엔터키를 누르지 않으면 return

--- src/js/index.js
+++ src/js/index.js
@@ -17,45 +17,45 @@ function App() {
 
     // 메뉴의 이름을 입력받는건
     $("#espresso-menu-name").addEventListener("keypress", (e)=>{
-        if(e.key === "Enter") {
-            const espressMenuName = $("#espresso-menu-name").value;
-            if($("#espresso-menu-name").value === "") { 
-                alert("값을 입력해주세요");
-                return;
-            }
-
-            const menuItemTemplate = (espressMenuName) => { return `<li class="menu-list-item d-flex items-center py-2">
-                <span class="w-100 pl-2 menu-name">${espressMenuName}</span>
-                <button
-                type="button"
-                class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-                >
-                수정
-                </button>
-                <button
-                type="button"
-                class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-                >
-                삭제
-                </button>
-            </li>`};
-            /* 
-            <!-- beforebegin -->
-                <ul>
-            <!-- afterbegin -->
-                <li></li>
-            <!-- beforeend -->
-                </ul>
-            <!-- afterend -->
-            */
-            $("#espresso-menu-list").insertAdjacentHTML(
-                "beforeend",
-                menuItemTemplate(espressMenuName)
-            );
-            const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
-            $(".menu-count").innerText = `총 ${menuCount}개`
-            $("#espresso-menu-name").value = "";
+        if(e.key !== "Enter") { 
+            return; 
         }
+        if($("#espresso-menu-name").value === "") { 
+            alert("값을 입력해주세요");
+            return;
+        }
+        const espressMenuName = $("#espresso-menu-name").value;
+        const menuItemTemplate = (espressMenuName) => { return `<li class="menu-list-item d-flex items-center py-2">
+            <span class="w-100 pl-2 menu-name">${espressMenuName}</span>
+            <button
+            type="button"
+            class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+            >
+            수정
+            </button>
+            <button
+            type="button"
+            class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+            >
+            삭제
+            </button>
+        </li>`};
+        /* 
+        <!-- beforebegin -->
+            <ul>
+        <!-- afterbegin -->
+            <li></li>
+        <!-- beforeend -->
+            </ul>
+        <!-- afterend -->
+        */
+        $("#espresso-menu-list").insertAdjacentHTML(
+            "beforeend",
+            menuItemTemplate(espressMenuName)
+        );
+        const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+        $(".menu-count").innerText = `총 ${menuCount}개`
+        $("#espresso-menu-name").value = "";
     });
 }
 
