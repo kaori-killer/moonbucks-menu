aa27a6b6f9c4bd5a4b879c5874df87e2e6a0d743
kaori-killer <sksbthwjd@daum.net>
2023-02-20 22:28:38 +0900

    feat: 확인 버튼을 클릭하면 메뉴가 삭제된다.

--- src/js/index.js
+++ src/js/index.js
@@ -8,17 +8,30 @@
 // - [x] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
 // - [x] 사용자 입력값이 빈 값이라면 추가되지 않는다.
 
+// TODO 메뉴 수정
+// - [x] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하는 모달창(prompt)이 뜬다.
+// - [x] 모달창에서 신규메뉴명을 입력 받고, 확인 버튼을 누르면 메뉴가 수정된다.    
+    
+
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
-    // TODO 메뉴 수정
-    // - [x] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하는 모달창(prompt)이 뜬다.
-    // - [x] 모달창에서 신규메뉴명을 입력 받고, 확인 버튼을 누르면 메뉴가 수정된다.    
+    // TODO 메뉴 삭제
+    // - [x] 메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌 모달창이 뜬다.
+    // - [x] 확인 버튼을 클릭하면 메뉴가 삭제한다.
+    // - [ ] 총 메뉴 갯수를 count하여 상단에 보여준다.
+
     $("#espresso-menu-list").addEventListener("click", (e)=>{
+        console.log(e.target.classList);
         if(e.target.classList.contains("menu-edit-button")){
             const $menuName = e.target.closest("li").querySelector(".menu-name");
             const newMenuName = prompt("메뉴명을 수정하세요",  $menuName.innerText);
             $menuName.innerText = newMenuName;
+        }
+        if(e.target.classList.contains("menu-remove-button")){
+            if(confirm("정말 삭제하시겠습니까?")){
+                e.target.closest("li").remove();
+            }
         }
     })
     
@@ -69,8 +82,3 @@ function App() {
 }
 
 App();
-
-// TODO 메뉴 삭제
-// - [ ] 메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌 모달창이 뜬다.
-//   - [ ] 확인 버튼을 클릭하면 메뉴가 삭제ㅚㄴ다.
-// - [ ] 총 메뉴 갯수를 count하여 상단에 보여준다.
