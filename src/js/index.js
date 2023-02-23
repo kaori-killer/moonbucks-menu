28b36ab1fcd0843f62c13c6cd60095bc5ada8f94
kaori-killer <sksbthwjd@daum.net>
2023-02-23 17:44:41 +0900

    feat: localStorage에 있는 데이터를 읽어온다

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,11 @@
 // step2 요구사항 구현을 위한 전략
 
 // TODO localStorage Read & Write
-// - [] localStorage에 데이터를 저장한다.
+// - [x] localStorage에 데이터를 저장한다.
 //  - [x] 메뉴를 추가할 때
 //  - [x] 메뉴를 수정할 때
 //  - [x] 메뉴를 삭제할 때
-// - [] localStorage에 있는 데이터를 읽어온다.
+// - [x] localStorage에 있는 데이터를 읽어온다.
 
 // TODO 카테고리별 메뉴판 관리
 // - [] 에스프레소 메뉴판을 관리한다.
@@ -30,13 +30,43 @@ const store = {
         localStorage.setItem("menu", JSON.stringify(menu));
     },
     getLocalStorage() {
-        localStorage.getItem("menu");
+        return JSON.parse(localStorage.getItem("menu"));
     },
 }
 
 function App() {
     // 상태[변하는 데이터, 이 앱에서 변하는 것이 무엇인가] - 메뉴명(->개수)
-    let menu = [];
+    this.menu = [];
+    this.init = () => {
+        if(store.getLocalStorage().length > 1) { this.menu = store.getLocalStorage(); }
+        render();
+    }
+
+    const render = () => {
+        const template = this.menu
+        .map((item, index) => {
+            return ( 
+                    `<li data-menu-id=${index} class="menu-list-item d-flex items-center py-2">
+                    <span class="w-100 pl-2 menu-name">${item.name}</span>
+                    <button
+                    type="button"
+                    class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+                    >
+                    수정
+                    </button>
+                    <button
+                    type="button"
+                    class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+                    >
+                    삭제
+                    </button>
+                </li>` 
+            );
+        })
+        .join("");
+
+        $("#espresso-menu-list").innerHTML = template;
+    }
 
     const countMenu = () => {
         const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
@@ -49,31 +79,10 @@ function App() {
             return;
         }
         const espressMenuName = $("#espresso-menu-name").value;
-        menu.push({ name: espressMenuName });
-        store.setLocalStorage(menu);
-        const template = menu
-            .map((item, index) => {
-                return ( 
-                        `<li data-menu-id=${index} class="menu-list-item d-flex items-center py-2">
-                        <span class="w-100 pl-2 menu-name">${item.name}</span>
-                        <button
-                        type="button"
-                        class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-                        >
-                        수정
-                        </button>
-                        <button
-                        type="button"
-                        class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-                        >
-                        삭제
-                        </button>
-                    </li>` 
-                );
-            })
-            .join("");
-        $("#espresso-menu-list").innerHTML = template;
+        this.menu.push({ name: espressMenuName });
+        store.setLocalStorage(this.menu);
         countMenu();
+        render();
         $("#espresso-menu-name").value = "";
     }
 
@@ -81,17 +90,17 @@ function App() {
         const menuId = e.target.closest("li").dataset.menuId;
         const $menuName = e.target.closest("li").querySelector(".menu-name");
         const newMenuName = prompt("메뉴명을 수정하세요",  $menuName.innerText);
-        menu[menuId] = newMenuName;
-        store.setLocalStorage(menu);
+        this.menu[menuId] = newMenuName;
+        store.setLocalStorage(this.menu);
         $menuName.innerText = newMenuName;
     }
 
     const RemoveMenuName = (e) => {
         if(confirm("정말 삭제하시겠습니까?")){
             const menuId = e.target.closest("li").dataset.menuId;
-            menu.splice(menuId, 1);
+            this.menu.splice(menuId, 1);
             e.target.closest("li").remove();
-            store.setLocalStorage(menu);
+            store.setLocalStorage(this.menu);
             countMenu();
         }
     }
@@ -117,6 +126,16 @@ function App() {
         }
         addMenuName();
     });
+
+    $("nav").addEventListener("click", (e)=>{
+        const isCategoryButton = e.target.classList.contains("cafe-category-name")
+        if(isCategoryButton) {
+            const categoryName = e.target.dataset.categoryName;
+            console.log(categoryName);
+        }
+    })
+
 }
 
-App();
+const app = new App();
+app.init();
